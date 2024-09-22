4061c6fd20230f48456755da9285a1f26d57cfb0
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-09-22 01:20:18 -0300

    feat(Schemas): Adding schemas package to the package

---
+++ schemas/file_schema.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from pydantic import BaseModel
+from pydantic import constr
+from pydantic import field_validator
+
+
+class FileMetadata(BaseModel):
+    file_name: constr(min_length=1)  # type: ignore
+    content_type: Optional[str]
+
+    @field_validator("content_type")
+    def check_content_type(cls, extension):
+        allowed_video_types = [
+            "video/mp4",
+            "video/x-matroska",  # .mkv
+            "video/avi",
+            "video/webm",
+            "video/ogg",
+        ]
+        if extension not in allowed_video_types:
+            raise ValueError("File Type not allowed, please send a video file")
+        return extension
+
+
+class QueueMessage(FileMetadata):
+    mp3_filename: Optional[str]
